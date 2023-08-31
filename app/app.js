cb971bc7c609db6e4c5e5103be844f98510c96ab
hyeonseo2006 <hyeonseoalex@gmail.com>
2023-08-31 01:37:30 -0700

    서버 구조화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict"
+
+// 모듈
+const express = require("express");
+const app = express();
+
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+//라우팅
+const home = require("./src/routes/home")
+app.use("/", home);
+
+module.exports = app;
