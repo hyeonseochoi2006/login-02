12644dbd199b00139b95c15689f161c5b1c9af9d
hyeonseo2006 <hyeonseoalex@gmail.com>
2023-09-01 12:20:35 -0700

    로그인 과 비밀번호 등록

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 "use strict";
 
-console.log("hello");
-console.log("bye")
+const id = document.querySelector("#nameInput"),
+psword = document.querySelector("#psword"),
+loginBtn = document.querySelector("#btn")
+
+console.log(id);
+
+loginBtn = addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    }
+}
