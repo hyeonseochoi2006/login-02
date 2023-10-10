ad1173e035c8c5b5d8868c45e4b64ffb654d3b5c
hyeonseo2006 <hyeonseoalex@gmail.com>
2023-10-09 19:20:43 -0700

    fs 모둘을 이용한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -14,9 +14,10 @@ class UserStorage {
         
         return userInfo;
     }
-    
-    static getUsers(...fields){
-        // const users = this.#users;
+
+    static #getUsers(data, isAll, fields){
+        const users = JSON.parse(data);
+        if (isAll) return users;
         const newUsers = fields.reduce((newUsers, fields) =>{
             if (users.hasOwnProperty(fields)){
                 newUsers[fields] = users[fields];
@@ -24,6 +25,16 @@ class UserStorage {
             return newUsers;
         }, {});
         return newUsers;
+    }
+    
+    static getUsers(isAll,...fields){
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+           return this.#getUsers(data, isAll, fields)
+            })
+            .catch(console.error);
+        
     }
 
     static getUserInfo(id) {
@@ -37,12 +48,17 @@ class UserStorage {
     
     
 
-    static save(userInfo) {
-        // const users = this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.psword.push(userInfo.psword);
-        return { success: true };
+    static async save(userInfo) {
+        const users =  await this.getUsers(true);
+        if(users.id.includes(userInfo.id)){
+            throw "이미 존재하는 아이디 입니다";
+        }
+        users.id.push(userInfo.id)
+        users.name.push(userInfo.name)
+        users.psword.push(userInfo.psword)
+        //데이터 추가
+       fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+       return { success: true }
     }
 }
 
